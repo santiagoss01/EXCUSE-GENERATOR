8d4effd02e1226a0b238d180843e1e6cba7d9450
santiagoss01 <santiagozehcnas@gmail.com>
2022-10-09 11:33:01 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btton").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = [
+    "barfed all over me",
+    "pissed my pants",
+    "crushed my nipple",
+    "broked in two"
+  ];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+  let randomExcuse = randomwhen.concat(" " + randomWho + " " + randomWhat);
+
+  return randomExcuse;
 };
